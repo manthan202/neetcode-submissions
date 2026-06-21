9b62b465875653341c03a971b9eb852461b47213
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-06-21 19:55:11 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0, j=s.length()-1;
+        while (j>i) {
+            while ((!isalnum(s[i]) || s[i]==' ') && i<j) i++;
+            while ((!isalnum(s[j]) || s[j]==' ') && i<j) j--;
+            if (tolower(s[i++])!=tolower(s[j--])) return false;
+        }
+        return true;
+    }
+};
