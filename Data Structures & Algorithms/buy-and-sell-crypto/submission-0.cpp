9d52993494f10331d9999f0a34be624917870659
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-06-23 20:04:15 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprofit=0;
+        for (int i=0; i<prices.size(); i++) {
+            for (int j=i+1; j<prices.size(); j++) {
+                maxprofit=max(maxprofit,prices[j]-prices[i]);
+            }
+        }
+        return maxprofit;
+    }
+};
