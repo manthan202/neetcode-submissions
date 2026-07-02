f65fb00fac33e2a1c2e4f0e1f20407cda7012bd5
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-07-02 21:08:31 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,44 @@
+class TrieNode {
+    public:
+        unordered_map<char, TrieNode*> children;
+        bool endOfWord=false;
+};
+
+class PrefixTree {
+    TrieNode* root;
+
+public:
+    PrefixTree() {
+        root=new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curr=root;
+        for (char c : word) {
+            if(curr->children.find(c)==curr->children.end()) {
+                curr->children[c]=new TrieNode();
+            }
+            curr=curr->children[c];
+        }
+        curr->endOfWord=true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr=root;
+        for(char c : word) {
+            if(curr->children.find(c)==curr->children.end()) return false;
+            curr=curr->children[c];
+        }
+        if (curr->endOfWord!=true) return false;
+        return true;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr=root;
+        for(char c : prefix) {
+            if(curr->children.find(c)==curr->children.end()) return false;
+            curr=curr->children[c];
+        }
+        return true;
+    }
+};
