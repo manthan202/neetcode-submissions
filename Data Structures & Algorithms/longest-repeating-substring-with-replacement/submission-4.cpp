806fb5dba97981c1de3d88d1f94b355198cbefed
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-07-18 00:33:08 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> hashArray(26,0);
+        int left=0, right=0, maxfreq=1;
+        int maxlen=min((int)s.length(), k);
+        while(right<s.length()){
+            hashArray[s[right]-'A']++;
+            maxfreq = max(maxfreq, hashArray[s[right]-'A']);
+            int changes=right-left+1-maxfreq;
+            if(changes>k) {
+                left++;
+                right++;
+                hashArray[s[left-1]-'A']--;
+                continue;
+            }
+            maxlen=max(maxlen, right-left+1);
+            
+            right++;
+        }
+        return maxlen;
+    }
+};
