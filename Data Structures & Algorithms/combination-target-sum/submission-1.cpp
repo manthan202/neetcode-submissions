9439460ff27a5430b3304b9aeb6bd06c3e9efd1e
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-07-20 20:14:37 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> current;
+        backTracking(nums, target, 0, current);
+        return result;
+    }
+
+    void backTracking(vector<int>& nums, int target, int start, vector<int>& current){
+        for(int i=start; i<nums.size(); i++){
+            if(nums[i]==target) {
+                current.push_back(nums[i]);
+                result.push_back(current);
+                current.pop_back();
+            }else if (nums[i]<target){
+                current.push_back(nums[i]);
+                backTracking(nums, target-nums[i], i, current);
+                current.pop_back();
+            }
+        }
+    }
+};
