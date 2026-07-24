ef32773ff91afe19836fd3f41afb79a7faa92292
Manthan Kapoor <manthan24336@iiitd.ac.in>
2026-07-24 23:40:28 +0530

    Add: coin-change - submission-11

---
+++ Data Structures & Algorithms/coin-change/submission-11.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount==0) return 0;
+        vector<int> DP(amount+1, amount+1); // size is amount+1 and element values is also amount+1 since we are going to take min of lengths and they will always be less than amount+1 since denominations are not below 1 
+        //DP[i] min number of coins needed to make up amount i
+
+        if(coins[0]>amount) return -1;
+        DP[0]=0;
+        DP[coins[0]]=1;
+
+        for(int i=coins[0]+1; i<=amount; i++){
+            for(int j=0; j<coins.size(); j++){
+                if (coins[j]<=i){
+                    DP[i]=min(DP[i-coins[j]]+1, DP[i]);
+                }
+            }
+        }
+        
+        if(DP[amount]==amount+1) return -1;
+        return DP[amount];
+    }
+};
